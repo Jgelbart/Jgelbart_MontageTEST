da4674d0a9c8fe87129a06e1ac5ce72f1a2afafa
Jody Morris Gelbart <sleenard@gmail.com>
2014-11-24 07:38:54 +0000

    Add component Jgelbart_test2

---
+++ ui/jgelbart_test2.reel/jgelbart_test2.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/jgelbart_test2.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Jgelbart_test2
+ * @extends Component
+ */
+exports.Jgelbart_test2 = Component.specialize(/** @lends Jgelbart_test2# */ {
+    constructor: {
+        value: function Jgelbart_test2() {
+            this.super();
+        }
+    }
+});
